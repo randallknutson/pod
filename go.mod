715833c89585a01cfa6911d0e2395ac608f59dee
Andrei Vereha <avereha@gmail.com>
2021-02-08 17:14:06 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/avereha/pod
+
+go 1.15
+
+require (
+	github.com/muka/go-bluetooth v0.0.0-20201211051136-07f31c601d33
+	github.com/sirupsen/logrus v1.6.0
+)
---
+++ main.go
@@ -0,0 +1,215 @@
+package main
+
+import (
+	"time"
+
+	"github.com/muka/go-bluetooth/api/service"
+	"github.com/muka/go-bluetooth/bluez/profile/agent"
+	"github.com/muka/go-bluetooth/bluez/profile/gatt"
+	"github.com/muka/go-bluetooth/hw"
+	log "github.com/sirupsen/logrus"
+	//	"github.com/muka/go-bluetooth/api/service"
+	//	"github.com/muka/go-bluetooth/bluez/profile/agent"
+	//	"github.com/muka/go-bluetooth/bluez/profile/gatt"
+)
+
+func btInit(adapterID string) {
+
+	btmgmt := hw.NewBtMgmt(adapterID)
+
+	// set LE mode
+	btmgmt.SetPowered(false)
+	btmgmt.SetLe(true)
+	btmgmt.SetBredr(false)
+	btmgmt.SetPowered(true)
+}
+
+func serve(adapterID string) error {
+
+	options := service.AppOptions{
+		AdapterID:  adapterID,
+		AgentCaps:  agent.CapNoInputNoOutput,
+		UUIDSuffix: "-e3ed-4464-8b7e-751e03d0dc5f",
+		UUID:       "1a7e",
+	}
+
+	a, err := service.NewApp(options)
+	if err != nil {
+		return err
+	}
+	defer a.Close()
+
+	a.SetName("go_bluetooth")
+
+	log.Infof("HW address %s", a.Adapter().Properties.Address)
+
+	if !a.Adapter().Properties.Powered {
+		err = a.Adapter().SetPowered(true)
+		if err != nil {
+			log.Fatalf("Failed to power the adapter: %s", err)
+		}
+	}
+
+	service1, err := a.NewService("4024")
+	if err != nil {
+		return err
+	}
+
+	err = a.AddService(service1)
+	if err != nil {
+		return err
+	}
+
+	char1, err := service1.NewChar("2441")
+	if err != nil {
+		return err
+	}
+
+	char1.Properties.Flags = []string{
+		gatt.FlagCharacteristicRead,
+		gatt.FlagCharacteristicWrite,
+	}
+
+	char1.OnRead(service.CharReadCallback(func(c *service.Char, options map[string]interface{}) ([]byte, error) {
+		log.Warnf("GOT CHAR READ REQUEST 1")
+		return []byte{42}, nil
+	}))
+
+	char1.OnWrite(service.CharWriteCallback(func(c *service.Char, value []byte) ([]byte, error) {
+		log.Warnf("GOT CHAR WRITE REQUEST 1 %x", value)
+		return value, nil
+	}))
+
+	descr1, err := char1.NewDescr("2902")
+	if err != nil {
+		return err
+	}
+
+	descr1.Properties.Flags = []string{
+		gatt.FlagDescriptorRead,
+		gatt.FlagDescriptorWrite,
+	}
+
+	descr1.OnRead(service.DescrReadCallback(func(c *service.Descr, options map[string]interface{}) ([]byte, error) {
+		log.Warnf("GOT DESC READ REQUEST 1 %+v", options)
+		return []byte{42}, nil
+	}))
+
+	descr1.OnWrite(service.DescrWriteCallback(func(d *service.Descr, value []byte) ([]byte, error) {
+		log.Warnf("GOT DESC WRITE REQUEST 2 %x", value)
+		return value, nil
+	}))
+
+	err = char1.AddDescr(descr1)
+	if err != nil {
+		return err
+	}
+
+	err = service1.AddChar(char1)
+	if err != nil {
+		return err
+	}
+
+	char2, err := service1.NewChar("2442")
+	if err != nil {
+		return err
+	}
+
+	char2.Properties.Flags = []string{
+		gatt.FlagCharacteristicRead,
+		gatt.FlagCharacteristicWrite,
+	}
+
+	char2.OnRead(service.CharReadCallback(func(c *service.Char, options map[string]interface{}) ([]byte, error) {
+		log.Warnf("GOT char READ REQUEST 2")
+		return []byte{42}, nil
+	}))
+
+	char2.OnWrite(service.CharWriteCallback(func(c *service.Char, value []byte) ([]byte, error) {
+		log.Warnf("GOT char WRITE REQUEST 2")
+		return value, nil
+	}))
+
+	descr2, err := char2.NewDescr("2902")
+	if err != nil {
+		return err
+	}
+
+	descr2.Properties.Flags = []string{
+		gatt.FlagDescriptorRead,
+		gatt.FlagDescriptorWrite,
+	}
+
+	descr2.OnRead(service.DescrReadCallback(func(c *service.Descr, options map[string]interface{}) ([]byte, error) {
+		log.Warnf("GOT desc READ REQUEST 1 %+v", options)
+		return []byte{42}, nil
+	}))
+
+	descr2.OnWrite(service.DescrWriteCallback(func(d *service.Descr, value []byte) ([]byte, error) {
+		log.Warnf("GOT desc WRITE REQUEST 2 %x", value)
+		return value, nil
+	}))
+
+	err = char2.AddDescr(descr2)
+	if err != nil {
+		return err
+	}
+	err = service1.AddChar(char2)
+	if err != nil {
+		return err
+	}
+
+	err = a.Run()
+	if err != nil {
+		return err
+	}
+
+	log.Infof("Exposed service %s", service1.Properties.UUID)
+
+	timeout := uint32(6 * 3600) // 6h
+	adv := a.GetAdvertisement()
+	log.Infof("Adv data: %+v", adv)
+	adv.Discoverable = true
+	adv.ServiceUUIDs = []string{
+		"00004024-0000-1000-8000-00805f9b34fb",
+		"00002470-0000-1000-8000-00805f9b34fb",
+		"0000000a-0000-1000-8000-00805f9b34fb",
+		// id
+		"0000ffff-0000-1000-8000-00805f9b34fb",
+		"0000fffe-0000-1000-8000-00805f9b34fb",
+		// lot number, serial
+		"0000aaaa-0000-1000-8000-00805f9b34fb",
+		"0000aaaa-0000-1000-8000-00805f9b34fb",
+		"0000aaaa-0000-1000-8000-00805f9b34fb",
+		"0000aaaa-0000-1000-8000-00805f9b34fb",
+	}
+	// Service UUIDs advertisement format
+	// [40 24, 24 70, 00 0a, <<4, ID>>, <<5, Lot number>>, <<3, Serial>> ]
+
+	log.Infof("Advertising for %ds...", timeout)
+	cancel, err := a.Advertise(timeout)
+	if err != nil {
+		return err
+	}
+	adv = a.GetAdvertisement()
+	log.Infof("Adv data: %+v", adv)
+	defer cancel()
+
+	time.Sleep((time.Duration(timeout) * time.Second))
+	wait := make(chan bool)
+	go func() {
+		time.Sleep(time.Duration(timeout) * time.Second)
+		wait <- true
+	}()
+
+	<-wait
+
+	return nil
+}
+
+func main() {
+	log.SetLevel(log.DebugLevel)
+	adapterID := "hci0"
+	btInit(adapterID)
+	serve(adapterID)
+}
