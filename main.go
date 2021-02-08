0ea664a8b16d044354808b9d9494ebe684e31515
Andrei Vereha <avereha@gmail.com>
2021-02-08 20:45:13 +0100

    or not

--- main.go
+++ main.go
@@ -143,7 +143,6 @@ func serve(adapterID string) error {
 
 	dataCharacteristic.OnWrite(service.CharWriteCallback(func(c *service.Char, value []byte) ([]byte, error) {
 		log.Warnf("data WRITE REQUEST %x", value)
-		dataCharacteristic.Confirm()
 		return value, nil
 	}))
 
