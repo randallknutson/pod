7c143a821c533dc8128af589963e91581b4b54dc
marionbarker <marionbarker@earthlink.net>
2021-12-05 23:09:04 -0800

    new default: debug log level, -v for trace, -q for info

--- main.go
+++ main.go
@@ -14,11 +14,19 @@ import (
 func main() {
 	var stateFile = flag.String("state", "state.toml", "pod state")
 	var freshState = flag.Bool("fresh", false, "start fresh. not activated, empty state")
+	// if both verbose and quiet are chosen, e.g., -v -q, the verbose dominates
+	var traceLevel = flag.Bool("v", false, "verbose off by default, TraceLevel")
+	var infoLevel = flag.Bool("q", false, "quiet off by default, InfoLevel")
 
 	flag.Parse()
 
-
-	log.SetLevel(log.TraceLevel)
+	if *traceLevel {
+		log.SetLevel(log.TraceLevel)
+	} else if *infoLevel {
+		log.SetLevel(log.InfoLevel)
+	} else {
+		log.SetLevel(log.DebugLevel)
+	}
 
 	log.SetFormatter(&logrus.TextFormatter{
 		DisableQuote: true,
