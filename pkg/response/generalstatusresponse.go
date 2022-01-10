8d130f75d8b4a12d7fefdacecf8faa9b6dddd888
marionbarker <marionbarker@earthlink.net>
2022-01-10 10:38:55 -0800

    generalstatusresponse.go: update comments

--- pkg/response/generalstatusresponse.go
+++ pkg/response/generalstatusresponse.go
@@ -8,11 +8,15 @@ import (
 //   Note - this response cannot have immediate_bolus_active =   True
 //          if true, Loop will refuse to bolus and won't see green loop with simulator
 // Special cases are in statusresponseXXX modules
-//   PodProgress = 4: response, _ := hex.DecodeString("1D4400004034000003FF")
-//   PodProgress = 6: response, _ := hex.DecodeString("1D160016D000400023FF")
-//   PodProgress = 7: response, _ := hex.DecodeString("1D570016F011000023FF")
-//   PodProgress = 8: response, _ := hex.DecodeString("1D18001F7000000023FF")
-//   Default        : response, _ := hex.DecodeString("1D1800A02800000463FF")
+//   The PodProgress is updated in the command pkg in command.go
+//   PodProgress = 2: respond to 0x07 with 0x0115, versionresponse.go
+//   PodProgress = 3: respond to 0x03 with 0x011b, setuniqueidresponse.go
+//   PodProgress = 3: respond to 0x19 with 0x1d, generalstatusresponsebeforeprime.go
+//   PodProgress = 4: response to prime command, statusresponseprime.og
+//   PodProgress = 6: response to basal rate program, statusreponseschedule.go
+//   PodProgress = 7: response to insert command, statusresponseinsert.go
+//   PodProgress = 8: all other 0x1d responses, except deactivate, type2 and type3
+//   Default        : this function, generalstatusresponse.go
 
 // From actual pod messages, expected CRC: 0x00FB for seqNumber = 9 for a msgBody of:
 //    "1d58001cc014000013ff"
