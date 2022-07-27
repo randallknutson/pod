82c053186d0e19cb22b10bd3fc5514eb8d00f7a3
Randall Knutson <randall@knutson.io>
2022-07-27 17:17:40 -0500

    Add back cmd import.

--- pkg/bluetooth/bluetooth.go
+++ pkg/bluetooth/bluetooth.go
@@ -14,6 +14,7 @@ import (
 	"github.com/avereha/pod/pkg/message"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/paypal/gatt"
+	"github.com/paypal/gatt/linux/cmd"
 	log "github.com/sirupsen/logrus"
 )
 
